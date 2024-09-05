d41291872ef3dee834a177ee4c9d1b5f27eb9f70
gymfightback <gymfightback@gmail.com>
2024-09-06 06:56:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,42 @@ flatpickr("#date", {
     dateFormat: "Y-m-d",
     minDate: "today", // Prevent booking past dates
 });
+
+// Load the API client and auth2 library
+gapi.load('client:auth2', initClient);
+
+function initClient() {
+    gapi.client.init({
+        apiKey: 'AIzaSyCc_ZLLpvuQuPPRFT3vusZjIFuAzQp23yY',
+        clientId: '882703469598-s4a5ndcgg87uceq6v3035jot9ojoraui.apps.googleusercontent.com',
+        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+        scope: "https://www.googleapis.com/auth/calendar.readonly"
+    }).then(function () {
+        listUpcomingEvents();
+    }, function(error) {
+        console.log(JSON.stringify(error, null, 2));
+    });
+}
+
+function listUpcomingEvents() {
+    gapi.client.calendar.events.list({
+        'calendarId': 'primary', // Use your Google Calendar ID
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
+    }).then(function(response) {
+        var events = response.result.items;
+        if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+                var event = events[i];
+                var when = event.start.dateTime || event.start.date;
+                console.log(event.summary + ' (' + when + ')');
+            }
+        } else {
+            console.log('No upcoming events found.');
+        }
+    });
+}
+
